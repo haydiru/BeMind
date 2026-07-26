93f74e33f89f4584806473fd6bb9a11704802c95
Haidir Magribi <haydiru@gmail.com>
2026-07-26 18:22:09 +0700

    fix(stt): fix import path

--- lib/pages/generate_essay_page.dart
+++ lib/pages/generate_essay_page.dart
@@ -6,7 +6,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package02/speech_to_text/speech_to_text.dart' as stt;
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 import 'package:lucide_icons_flutter/lucide_icons.dart';
 import 'package:provider/provider.dart';
 import 'package:record/record.dart';
